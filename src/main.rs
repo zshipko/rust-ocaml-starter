da95e7d746009e2770562b97c62ea7a5d958d241
Eric Martin Christiansen <ericmartinchristiansen@gmail.com>
2023-01-20 17:39:57 +0000

    PR feedback

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,9 @@
 // Read the corresponding .ml file first.
 
-////////////////////////////////////////////////////////////////////////////////
-// The simple hello-world function.
 ocaml::import! {
     fn hello_world() -> String;
 }
 
-////////////////////////////////////////////////////////////////////////////////
-// The variant example.
 #[derive(ocaml::ToValue, ocaml::FromValue, Debug)]
 pub enum T {
     A,
@@ -18,10 +14,14 @@ ocaml::import! {
     fn maybe_inc(t: T) -> T;
 }
 
-////////////////////////////////////////////////////////////////////////////////
-// The first struct example.
-// NOTE: If the OCaml type `MyStruct.t` included a value we couldn't (or didn't
-// want to) wrap, we would use `ocaml::Value` for it.
+// We can optionally make input args references, allowing us to reuse a value
+// in Rust.
+ocaml::import! {
+    fn maybe_inc_ref(t: &T) -> T;
+}
+
+// If the OCaml type `MyStruct.t` included a value we couldn't (or didn't want
+// to) wrap, we would use `ocaml::Value` for it.
 #[derive(ocaml::ToValue, ocaml::FromValue, Debug)]
 pub struct MyStructT {
     a: ocaml::Int,
@@ -32,65 +32,13 @@ ocaml::import! {
     fn mystruct_inc_both(t: MyStructT) -> MyStructT;
 }
 
-////////////////////////////////////////////////////////////////////////////////
-// The tuple example.
-
-ocaml::import! {
-    fn tuple_inc_both(t: (ocaml::Int, ocaml::Float)) -> (ocaml::Int, ocaml::Float);
-}
-
-////////////////////////////////////////////////////////////////////////////////
-// The opaque type example.
-#[derive(ocaml::ToValue, ocaml::FromValue, Debug)]
-pub struct OpaqueT {
-    oc: ocaml::Value,
-}
-
-ocaml::import! {
-    fn opaque_create() -> OpaqueT;
-    // We can optionally make input args references, allowing us to reuse
-    // a value in Rust.
-    fn opaque_use(t: &OpaqueT) -> String;
-}
-
-////////////////////////////////////////////////////////////////////////////////
-// The BrokenStruct example.
-#[derive(ocaml::ToValue, ocaml::FromValue, Debug)]
-pub struct BrokenStructT {
-    a: ocaml::Float,
-    b: ocaml::Float,
-}
-
-ocaml::import! {
-    fn brokenstruct_inc_both(t: BrokenStructT) -> BrokenStructT;
-}
-
 fn main() {
     let gc = ocaml::runtime::init();
 
     unsafe {
-        println!("Simple example: {}", hello_world(&gc).unwrap());
-    }
-
-    unsafe {
-        println!("Variant example: {:?}", maybe_inc(&gc, T::B(1)).unwrap());
-    }
-
-    unsafe {
-        println!("First struct example: {:?}", mystruct_inc_both(&gc, MyStructT{a: 1, b: 2.0}).unwrap());
-    }
-
-    unsafe {
-        println!("Tuple example: {:?}", tuple_inc_both(&gc, (1, 2.0)).unwrap());
-    }
-
-    unsafe {
-        let opaque = opaque_create(&gc).unwrap();
-        println!("Opaque type example: {:?}", opaque_use(&gc, &opaque).unwrap());
-        println!("Opaque type example called again: {:?}", opaque_use(&gc, &opaque).unwrap());
-    }
-
-    unsafe {
-        println!("BrokenStruct example: {:?}", brokenstruct_inc_both(&gc, BrokenStructT{a: 1.0, b: 2.0}).unwrap());
+        println!("hello_world: {}", hello_world(&gc).unwrap());
+        println!("maybe_inc: {:?}", maybe_inc(&gc, T::B(1)).unwrap());
+        println!("maybe_inc_ref: {:?}", maybe_inc_ref(&gc, &T::B(1)).unwrap());
+        println!("mystruct_inc_both: {:?}", mystruct_inc_both(&gc, MyStructT{a: 1, b: 2.0}).unwrap());
     }
 }
